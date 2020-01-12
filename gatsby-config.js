e3e31f949c18ee82788917413b5d56dc54817477
Ben Davidson <bendavidson@Bens-MacBook-Pro.local>
2020-01-12 07:55:11 +0100

    Added environment

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,9 +23,9 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: "aa6lmvvj10ja",
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
         // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: "coEg-s-TeNE4MIZnenmUau8zdPNpZZQc5LqeJ9dW6qU",
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     `gatsby-transformer-sharp`,
